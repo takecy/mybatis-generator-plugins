18bb456a610bdb771b138b9d19959a5ee8554681
takecy <github@takebo.info>
2015-02-10 02:03:15 +0900

    init

---
+++ src/main/java/info/takebo/info/takebo/mybatis/plugin/App.java
@@ -0,0 +1,13 @@
+package info.takebo.info.takebo.mybatis.plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
